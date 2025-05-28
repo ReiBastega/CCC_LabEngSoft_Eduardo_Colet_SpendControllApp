1c2c89e52b6baa56a068f1db7212e3f104e80f4d
Eduardo Colet <eduardocolet5@gmail.com>
2025-05-27 22:36:40 -0300

    .

--- spend_controll/lib/modules/transactions/transaction_modules.dart
+++ spend_controll/lib/modules/transactions/transaction_modules.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:spend_controll/modules/auth/login/controller/login_controller.dart';
-import 'package:spend_controll/modules/auth/login/login_module.dart';
 import 'package:spend_controll/modules/transactions/add_expense/add_expense_module.dart';
 import 'package:spend_controll/modules/transactions/add_income/add_income_module.dart';
 import 'package:spend_controll/modules/transactions/transfer/transfer_module.dart';
@@ -18,7 +17,6 @@ class TransactionModules extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ModuleRoute('/', module: LoginModule()),
         ModuleRoute('/add-expense/', module: AddExpenseModule()),
         ModuleRoute('/add-income/', module: AddIncomeModule()),
         ModuleRoute('/transfer/', module: TransferModule()),
